152ea24999d83251378e8b65e07b894996fab026
DESKTOP-N5AIFHN\daw1 <icastellvia01@educarex.es>
2023-09-21 17:20:14 +0200

    castelar

---
+++ holamundo/src/App3.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("hola mundo!");
+        systm.out.println("nuevo")
+        systm.out.println("agua")
+        systm.out.println("castelar")
+    }
+}
