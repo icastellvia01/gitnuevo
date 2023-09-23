be4a2208edccb5a41fced24080a0abef486a542b
ismael <icastellvia01@educarex.es>
2023-09-23 19:12:57 +0200

    hola que tal

---
+++ holamundo/src/public class App {.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("hola que tal!");
+        System.out.println("clase nueva");
+    }
+}
